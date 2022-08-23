b85075a96d7955353a5d716372b30cac4473c046
phucminh.nguyen <nguyenphucminh.lx@gmail.com>
2022-08-23 14:47:53 +0700

    install format code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,47 @@
-const express = require('express')
-const morgan = require('morgan')
-const { engine } = require ('express-handlebars');
-const app = express()
-const port = 3000
-const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+const path = require('path');
 
 const routes = require('./routes');
 //STATIC PUBLIC FILE
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 //HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //TEMPLATE ENGINE
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
-app.set("views", path.join(__dirname, 'resources', 'views'));
+app.set(
+    'views',
+    path.join(
+        __dirname,
+        'resources',
+
+        'views',
+    ),
+);
 
 //PATH
 
-routes(app)
+routes(app);
 
 //CAN USE FORM DATA(BODY)
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 //PORT
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
